cf9c18fa5e67a0d4f664497b6fc9dee5570e6b0c
capitalist42 <capitalist42@protonmail.ch>
2023-09-01 16:12:29 +0800

    add ledger support

--- apps/web/craco.config.js
+++ apps/web/craco.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+const webpack = require('webpack');
 
 module.exports = {
   typescript: {
@@ -14,7 +15,6 @@ module.exports = {
   },
   webpack: {
     configure: config => {
-      
       config.module.rules.push({
         test: /\.tsx?$/,
         loader: 'ts-loader',
@@ -29,10 +29,19 @@ module.exports = {
         buffer: require.resolve('buffer'),
         crypto: require.resolve('crypto-browserify'),
         assert: require.resolve('assert'), 
+        https:  require.resolve('https-browserify'),
+        http: require.resolve('stream-http'), 
+        url: require.resolve('url'),
       };
       config.ignoreWarnings = [
         /Failed to parse source map/,
       ];
+
+      config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+        })
+      ]);
       return config;
     }
   },
---
+++ apps/web/src/lib/WalletConnector.ts
@@ -0,0 +1,31 @@
+
+import Onboard from '@sovryn/onboard-core';
+import { Asset, BasePath } from '@sovryn/onboard-hw-common';
+import injectedModule from '@sovryn/onboard-injected';
+import ledgerModule from '@sovryn/onboard-ledger';
+
+const basePaths: BasePath[] = [
+  { label: 'RSK Mainnet', value: "m/44'/137'/0'/0" },
+  { label: 'Ethereum Mainnet', value: "m/44'/60'/0'/0" },
+];
+const assets: Asset[] = [{ label: 'RBTC' }, { label: 'ETH' }];
+
+const injected = injectedModule();
+const ledger = ledgerModule({
+  basePaths,
+  assets,
+});
+const chains = [
+  {
+    id: '0x1f',
+    rpcUrl: 'https://public-node.testnet.rsk.co',
+    label: 'RSK Testnet',
+    token: 'tRBTC',
+    blockExplorerUrl: 'https://explorer.testnet.rsk.co',
+  },
+];
+
+export const onboard = Onboard({
+  wallets: [injected, ledger],
+  chains,
+});
