b7e36ff1d111df8e7505d418f7c6aca38755a55e
capitalist42 <capitalist42@protonmail.ch>
2023-09-03 17:17:08 +0800

    hide "Learn more"  link

--- apps/web/src/app/pages/GetStartedPage.tsx
+++ apps/web/src/app/pages/GetStartedPage.tsx
@@ -22,9 +22,9 @@ function GetStartedPage(): JSX.Element {
             text="Connect Wallet to Start"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           />
-          <a className="text-sm font-semibold leading-6 text-gray-900">
+          {/* <a className="text-sm font-semibold leading-6 text-gray-900">
             Learn more <span aria-hidden="true">→</span>
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
