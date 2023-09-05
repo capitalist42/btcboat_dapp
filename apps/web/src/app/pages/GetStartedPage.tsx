6a6e7c7ec04dd470b8173e4d42953b9c5a8a3478
capitalist42 <capitalist42@protonmail.ch>
2023-09-05 11:35:36 +0800

    change get start button color to orange

--- apps/web/src/app/pages/GetStartedPage.tsx
+++ apps/web/src/app/pages/GetStartedPage.tsx
@@ -20,7 +20,7 @@ function GetStartedPage(): JSX.Element {
           <SimpleConnectWalletButton
             onConnect={connectWallet}
             text="Connect Wallet to Start"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
           />
           <a href="https://capitalist42.github.io/heavensdoor.github.io/" className="text-sm font-semibold leading-6 text-gray-900">
             Learn more <span aria-hidden="true">→</span>
