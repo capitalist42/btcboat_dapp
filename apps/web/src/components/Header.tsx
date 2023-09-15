849e8c3881d60baf9b15487335bf448c53ca880c
capitalist42 <capitalist42@protonmail.ch>
2023-09-15 17:22:14 +0800

    remove unused import

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,6 +1,4 @@
-import { FC, Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { FC } from 'react'
 import React from 'react'
 
 interface HeaderProps {}
@@ -9,9 +7,9 @@ interface HeaderProps {}
 //     { name: 'Account', href: '/#/account', current: true },
 // ]
 
-function classNames(...classes:any) {
-    return classes.filter(Boolean).join(' ')
-}
+// function classNames(...classes:any) {
+//     return classes.filter(Boolean).join(' ')
+// }
 
 const Header: FC<HeaderProps> = (_props) => {
     return (
