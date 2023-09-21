439fc3357f2f1faa95be81a542fb5daace68416c
capitalist42 <capitalist42@protonmail.ch>
2023-09-21 13:59:15 +0800

    deploy account modal

--- apps/web/src/pages/DashboardPage.tsx
+++ apps/web/src/pages/DashboardPage.tsx
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useOnboardWalletHook } from "../hooks/useOnboardWalletHook";
 import { useIndividualAccountHook } from "../hooks/useIndividualAccountHook";
-import { PortfolioBalance } from '../components/PortfolioBalance';
+import { DeployAccountModal } from "../components/DeployAccountModal";
+import { PortfolioBalance } from "../components/PortfolioBalance";
 import { AssetsList } from "../components/AssetsList";
-import { Convert } from '../components/Convert';
-import { Lending } from '../components/Lending';
+import { Convert } from "../components/Convert";
+import { Lending } from "../components/Lending";
+
 function DashboardPage(): JSX.Element {
   const { firstAccountAddress } = useOnboardWalletHook();
   const { accountState } = useIndividualAccountHook();
+  const [isDeployAccountModalOpen, setOpenDeployAccountModal] = useState(false);
+  const firstIndividualAccountAddress = accountState.data[0]
+    ? accountState.data[0].address
+    : null;
+
   console.debug("firstAccountAddress", firstAccountAddress);
   console.debug("accountState", accountState);
+
+  useEffect(() => {
+    // check firstIndividualAccountAddress deployment status on chain
+    setOpenDeployAccountModal(true);
+  }, [accountState]);
+
   return (
     <>
+      <DeployAccountModal
+        individualAccountAddress={firstIndividualAccountAddress}
+        open={isDeployAccountModalOpen}
+        setOpen={setOpenDeployAccountModal}
+      />
       <div className="grid grid-cols-1 gap-4">
         <div className="">
-         <PortfolioBalance />
+          <PortfolioBalance />
         </div>
 
         <div className="">
           <AssetsList />
         </div>
       </div>
-      
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
         <div className="">
           <Convert />
