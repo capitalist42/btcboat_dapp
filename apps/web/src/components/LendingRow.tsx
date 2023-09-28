2d1d114168027501e05dcdf8904f3318da518656
capitalist42 <capitalist42@protonmail.ch>
2023-09-28 17:18:05 +0800

    add APY

--- apps/web/src/components/LendingRow.tsx
+++ apps/web/src/components/LendingRow.tsx
@@ -1,23 +1,24 @@
 import type { FC } from "react";
 import React, { useState, useEffect, useMemo } from "react";
 import { ChainId } from "@sovryn/ethers-provider";
-import { Contract, ContractInterface, utils } from "ethers";
-import { ContractGroup, getContract, SupportedTokens } from "@sovryn/contracts";
-import { ChainIds } from '@sovryn/ethers-provider';
-
+import { Contract, ContractInterface } from "ethers";
+import { getContract } from "@sovryn/contracts";
+import { Decimal } from "@sovryn/utils";
+import { ChainIds } from "@sovryn/ethers-provider";
 import { LendingPool } from "../utils/LendingPoolMap";
 import { useOnboardWalletHook } from "../hooks/useOnboardWalletHook";
+import { LendModal } from "./LendModal";
 
 interface LendingFrameProps {
   pool: LendingPool;
 }
 
 export const LendingRow: FC<LendingFrameProps> = ({ pool }) => {
-  const chainId: ChainId = ChainIds.RSK_TESTNET
+  const chainId: ChainId = ChainIds.RSK_TESTNET;
 
   const asset = useMemo(() => pool.getAsset(), [pool]);
   const { web3Provider } = useOnboardWalletHook();
-
+  const [lendModalOpen, setLendModalOpen] = useState<boolean>(false);
   const [contractAddress, setContractAddress] = useState<string | undefined>(
     undefined
   );
@@ -30,6 +31,8 @@ export const LendingRow: FC<LendingFrameProps> = ({ pool }) => {
   const [availableAmount, setAvailableAmount] = useState<string | undefined>(
     undefined
   );
+
+  const [apy, setAPY] = useState<string | undefined>(undefined);
   // const balance = await contract.totalAssetBorrow();
   // return utils.formatUnits(balance);
 
@@ -47,28 +50,50 @@ export const LendingRow: FC<LendingFrameProps> = ({ pool }) => {
   }, [asset, chainId]);
 
   useEffect(() => {
-    const fetchLendingPoolData = async() => {
+    const fetchLendingPoolData = async () => {
       if (contractAddress && contractAbi && web3Provider) {
-        const contract = new Contract(contractAddress, contractAbi, web3Provider);
+        const contract = new Contract(
+          contractAddress,
+          contractAbi,
+          web3Provider
+        );
         const totalAssetBorrowValue = await contract.totalAssetBorrow();
-        const totalAssetBorrow = utils.formatUnits(totalAssetBorrowValue);
+        const totalAssetBorrowDecimal = Decimal.fromBigNumberString(
+          totalAssetBorrowValue
+        );
+
         const availableAmountValue = await contract.marketLiquidity();
-        const availableAmount = utils.formatUnits(availableAmountValue);
-        setTotalAssetBorrow(totalAssetBorrow);
-        setAvailableAmount(availableAmount);
+        const availableAmountDecimal =
+          Decimal.fromBigNumberString(availableAmountValue);
+        const apyValue = await contract.nextSupplyInterestRate(0);
+        const apyDecimal = Decimal.fromBigNumberString(apyValue);
+
+        setTotalAssetBorrow(totalAssetBorrowDecimal.prettify(3));
+        setAvailableAmount(availableAmountDecimal.prettify(3));
+        setAPY(apyDecimal.prettify(3));
       }
-    }
+    };
     console.debug("fetchLendingPoolData");
-    fetchLendingPoolData()
+    fetchLendingPoolData();
   }, [contractAddress, contractAbi, web3Provider]);
+
+  const handleLendButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    setLendModalOpen(true);
+  };
 
   return (
     <>
+      <LendModal open={lendModalOpen} setOpen={setLendModalOpen} />
       <tr key={pool.getName()}>
         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
           {pool.getName()}
         </td>
-        <td className="whitespace-nowrap px-3 py-4 text-sm text-white">....</td>
+        <td className="whitespace-nowrap px-3 py-4 text-sm text-white">
+          {apy}%
+        </td>
         <td className="whitespace-nowrap px-3 py-4 text-sm text-white">
           {totalAssetBorrow}
         </td>
@@ -79,6 +104,7 @@ export const LendingRow: FC<LendingFrameProps> = ({ pool }) => {
           <button
             type="button"
             className="rounded bg-green-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+            onClick={handleLendButtonClick}
           >
             Lend
           </button>
--- apps/web/src/pages/OpenLocalAccount.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function OpenLocalAccountPage(): JSX.Element {
-  return <div>Open Local Accout Page...</div>;
-}
-
-export default OpenLocalAccountPage;
